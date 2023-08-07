3b98c27914a7a9a42a611c494cd487850867cf19
Varun Kumar <3varun0@gmail.com>
2023-08-07 14:02:04 +0530

    Checking in code changes for Test.java changes

--- DSA/com/vk/dsa/Test.java
+++ DSA/com/vk/dsa/Test.java
@@ -3,101 +3,126 @@ package com.vk.dsa;
 import java.util.*;
 
 public class Test {
-    public static void main(String[] args) {    
+    public static void main(String[] args) {
+        MySingleLinkedList ll = new MySingleLinkedList();
 
-        //sort the array
-        int [] nums = {1,3,2,4,56,67,7,7,78,8};
+        ll.addAtTail(1);
+        ll.addAtTail(2);
+        ll.addAtTail(3);
+        //ll.addAtTail(4);
+        //ll.addAtTail(5);
 
-        //sort the array
+        ll.head = ll.reverseBetween(ll.head, 1, 2);
 
-        Arrays.sort(nums);
-        Arrays.stream(nums).forEach(i -> System.out.println(i));
-        System.exit(0);
-
-        MyCircularQueue st = new MyCircularQueue(3);
-        System.out.println(st.enQueue(1));
-        System.out.println(st.enQueue(2));
-        System.out.println(st.enQueue(3));
-        System.out.println(st.enQueue(4));
-        System.out.println(st.Rear());
-        System.out.println(st.isFull());
-        System.out.println(st.deQueue());
-        System.out.println(st.enQueue(4));
-        System.out.println(st.Rear());
+        //printing
+        ListNode cur = ll.head;
+        while (cur != null) {
+            System.out.println(cur.val+", ");
+            cur = cur.next;
+        }
 
     }
 }
 
-class MyCircularQueue {
+class ListNode {
 
-    int [] data;
-    int curSize;
-    int maxSize;
-    int front;
-    int rear;
+    int val;
+    ListNode next;
 
-    public MyCircularQueue(int k) {
-        maxSize = k;
-        data = new int[maxSize];
-        curSize = 0;
-        front = 0;
-        rear = 0;
+    public ListNode(int val) {
+        this.val = val;
     }
 
-    public boolean enQueue(int value) {
-        if (isFull()) {
-            return false;
-        } else {
-            data[curSize] = value;
-            rear = ((rear+1) % maxSize);
-            curSize++;
-            return true;
-        }
-    }
 
-    public boolean deQueue() {
-        if (isEmpty()) {
-            System.out.println("CircularQueue is empty, there is no element to remove.");
-            return false;
-        } else {
-            front = ((front+1) % maxSize);
-            curSize--;
-            return true;
-        }
-    }
-
-    public int Front() {
-        if (isEmpty()) {
-            return -1;
-        } else {
-            return data[front-1];
-        }
-    }
-
-    public int Rear() {
-        if (isEmpty()) {
-            return -1;
-        } else {
-            return data[curSize-1];
-        }
-    }
-
-    public boolean isEmpty() {
-        return curSize == 0;
-    }
-
-    public boolean isFull() {
-        return curSize == maxSize;
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
 
 /**
- * Your MyCircularQueue object will be instantiated and called as such:
- * MyCircularQueue obj = new MyCircularQueue(k);
- * boolean param_1 = obj.enQueue(value);
- * boolean param_2 = obj.deQueue();
- * int param_3 = obj.Front();
- * int param_4 = obj.Rear();
- * boolean param_5 = obj.isEmpty();
- * boolean param_6 = obj.isFull();
+ *
+ * Problem 27 : addAtTail(int val);
  */
+class MySingleLinkedList {
+
+    ListNode head;
+
+    public MySingleLinkedList() {
+        head = null;
+    }
+
+    /**
+     * Problem 27
+     *
+     * @param val
+     */
+    public void addAtTail(int val) {
+        ListNode newNode = new ListNode(val, null);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        ListNode current = head;
+
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+
+        if (head == null || head.next == null || left == right) {
+            return head;
+        }
+
+        ListNode cur = head;
+        ListNode prev = null;
+        ListNode next = null;
+
+        ListNode leftPartPrev = null;
+        ListNode revEnd = null;
+        boolean isRevStart = false;
+        int pos = 0;
+
+        while (cur != null) {
+            pos++;
+            if (pos == left) {
+                isRevStart = true;
+                revEnd = cur;
+            }
+
+            if (isRevStart) {
+                //rev start
+                next = cur.next;
+                cur.next = prev;
+                prev = cur;
+                cur = next;
+                //rev end
+                if (pos == right) {
+                    break;
+                }
+            } else {
+                leftPartPrev = cur;
+                cur = cur.next;
+            }
+        }
+
+        if (isRevStart) {
+            if (left == 1) {
+                if (next == null) {//is entire list is reversed
+                    return prev;
+                }
+                head = prev;
+            } else {
+                leftPartPrev.next = prev;
+            }
+            revEnd.next = next;
+        }
+        return head;
+
+    }
+}
